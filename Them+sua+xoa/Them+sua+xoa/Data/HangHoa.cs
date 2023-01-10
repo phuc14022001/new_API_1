5bf2d93d071363512065e5570712251dfac5a9a9
phuc14022001 <106235647+phuc14022001@users.noreply.github.com>
2023-01-10 16:24:37 +0700

    API_1

---
+++ Them+sua+xoa/Them+sua+xoa/Data/HangHoa.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Them_sua_xoa.Data
+{
+    public class HangHoa
+    {
+        [Key]
+        public Guid maHang { get; set; }
+        [Required]
+        [MaxLength(100)]
+        public string tenHang { get; set; }
+
+        public string moTa { get; set; }
+
+        [Range(0,double.MaxValue)] 
+        public double donGia { get; set; }
+        public byte giamGia { get; set; }
+        public int? maLoai { get; set; }
+        [ForeignKey("maLoai")]
+        public Loai Loai { get; set; }
+    }
+}
---
+++ Them+sua+xoa/Them+sua+xoa/Models/LoaiModel.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Them_sua_xoa.Models
+{
+    public class LoaiModel
+    {
+        [Required]
+        [MaxLength(50)]
+        public string tenLoai { get; set; }
+    }
+}
